7bf9113e24d812b82faf75c19fd6b659a2552e9f
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-10-25 01:01:22 -0400

    added code for project

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
+"use client"; // this indicates that the home component is a CSR component
+
+import styled from "styled-components";
+import { useState } from "react";
+import Link from "next/link";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export default function Home() {
-  return <></>;
+  const [city, setCity] = useState("");
+
+  return (
+    <StyledDiv>
+      <h1>Find the Weather in Any City!</h1>
+      <p>Enter a city name below to get the current weather.</p>
+      <input
+        type="text"
+        value={city}
+        placeholder="City name"
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <Link href={`/${city}`}>Get Weather</Link>
+    </StyledDiv>
+  );
 }
